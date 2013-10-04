d9a00364f533ab4b41d82fa1b69083cd0e0cddbc
Bertrand Paquet <bpaquet@octo.com>
2013-10-04 14:42:19 +0200

    Remove debug

--- cookbooks/topology/recipes/set_hostname.rb
+++ cookbooks/topology/recipes/set_hostname.rb
@@ -4,11 +4,14 @@ service "hostname" do
   action auto_compute_action
 end
 
+ohai "reload_hostname" do
+  action :nothing
+  plugin "hostname"
+end
 
-p node[:topology_node_name]
-p node.topology
 file "/etc/hostname" do
   content node.topology[node[:topology_node_name]][:topology_hostname]
   mode '0644'
   notifies :restart, "service[hostname]"
+  notifies :run, "ohai[reload_hostname]"
 end
