f25bbfe9cdc31450bb81494d2398e261b50b0ca9
Bertrand Paquet <bertrand.paquet@gmail.com>
2013-10-13 21:52:39 +0200

    Update lxc driver

--- lib/master-cap/hypervisors/lxc.rb
+++ lib/master-cap/hypervisors/lxc.rb
@@ -9,7 +9,7 @@ class HypervisorLxc < Hypervisor
   def initialize(cap, params)
     super(cap, params)
     @params = params
-    [:lxc_user, :lxc_host, :lxc_sudo, :template, :vg_name, :ssh_keys].each do |x|
+    [:lxc_user, :lxc_host, :lxc_sudo].each do |x|
       raise "Missing params :#{x}" unless @params[x]
     end
     @ssh = SshDriver.new @params[:lxc_host], @params[:lxc_user], @params[:lxc_sudo]
@@ -40,9 +40,30 @@ class HypervisorLxc < Hypervisor
     return unless no_dry
     l.each do |name, vm|
       ip_config = vm[:host_ips][:admin]
-      puts "Creating #{name} (#{ip_config[:ip]})"
-      gateway = @ssh.capture("/bin/sh -c '. /etc/default/lxc && echo \\$LXC_ADDR'").strip
-      netmask = @ssh.capture("/bin/sh -c '. /etc/default/lxc && echo \\$LXC_NETMASK'").strip
+      template_name = vm[:vm][:template_name] || @params[:default_vm][:template_name]
+      template_opts = vm[:vm][:template_opts] || @params[:default_vm][:template_opts] || ""
+      raise "No template specified for vm #{name}" unless template_name
+      puts "Creating #{name}, using template #{template_name}, options #{template_opts}"
+      network_gateway = vm[:vm][:network_gateway] || @params[:default_vm][:network_gateway] || @ssh.capture("/bin/sh -c '. /etc/default/lxc && echo \\$LXC_ADDR'").strip
+      network_netmask = vm[:vm][:network_netmask] || @params[:default_vm][:network_netmask] || @ssh.capture("/bin/sh -c '. /etc/default/lxc && echo \\$LXC_NETMASK'").strip
+      network_bridge = vm[:vm][:network_bridge] || @params[:default_vm][:network_bridge] || @ssh.capture("/bin/sh -c '. /etc/default/lxc && echo \\$LXC_BRIDGE'").strip
+      network_dns = vm[:vm][:network_dns] || @params[:default_vm][:network_dns] || network_gateway
+      puts "Network config for #{name} : #{ip_config[:ip]} / #{network_netmask}, gateway #{network_gateway}, bridge #{network_bridge}, dns #{network_dns}"
+
+      ssh_keys = []
+      ssh_keys += @params[:default_vm][:ssh_keys] if @params[:default_vm][:ssh_keys]
+      ssh_keys += vm[:vm][:ssh_keys] if vm[:vm][:ssh_keys]
+
+      lvm_mode = vm[:vm][:lvm] || @params[:default_vm][:lvm]
+
+      if lvm_mode
+        lvm_vg = vm[:vm][:lvm][:vg_name] || @params[:default_vm][:lvm][:vg_name]
+        lvm_size = vm[:vm][:lvm][:root_size] || @params[:default_vm][:lvm][:root_size]
+        raise "No vg for #{name}" unless lvm_vg
+        raise "No size for #{name}" unless lvm_size
+      end
+
+      user = @cap.fetch(:user)
 
       iface = <<-EOF
 auto lo
@@ -51,48 +72,51 @@ iface lo inet loopback
 auto eth0
 iface eth0 inet static
   address #{ip_config[:ip]}
-  netmask #{netmask}
-  gateway #{gateway}
+  netmask #{network_netmask}
+  gateway #{network_gateway}
 EOF
-      @ssh.scp "/tmp/iface", iface
 
       config = []
+
+      config << "lxc.network.type = veth"
+      config << "lxc.network.link = #{network_bridge}"
+      config << "lxc.network.flags = up"
+      config << ""
       config << "lxc.cgroup.memory.limit_in_bytes = #{vm[:vm][:memory]}" if vm[:vm][:memory]
       config << "lxc.cgroup.memory.memsw.limit_in_bytes = #{vm[:vm][:memory_swap]}" if vm[:vm][:memory_swap]
       config << "lxc.cgroup.cpu.shares = #{vm[:vm][:cpu_shares]}" if vm[:vm][:cpu_shares]
       config << ""
-      @ssh.scp "/tmp/config", config.join("\n")
-      command = "lxc-create -t #{@params[:template]} -n #{name} -B lvm --vgname #{@params[:vg_name]}"
-      command += " --fssize #{vm[:vm][:root_disk]}"
+      config << ""
+      @ssh.scp "/tmp/lxc_config", config.join("\n")
+      command = "lxc-create -t #{template_name} -n #{name} -f /tmp/lxc_config"
+      command += " -B lvm --vgname #{lvm_vg} --fssize #{lvm_size}" if lvm_mode
+      command += " -- #{template_opts}"
+      puts command
       @ssh.exec command
-      @ssh.exec "cat /tmp/config | sudo tee -a /var/lib/lxc/#{name}/config"
-      @ssh.exec "mount /dev/#{@params[:vg_name]}/#{name} /var/lib/lxc/#{name}/rootfs"
+      @ssh.exec "mount /dev/#{lvm_vg}/#{name} /var/lib/lxc/#{name}/rootfs" if lvm_mode
       @ssh.exec "rm -f /var/lib/lxc/#{name}/rootfs/etc/ssh/ssh_host*key*"
-      @ssh.exec "ssh-keygen -t rsa -f /var/lib/lxc/#{name}/rootfs/etc/ssh/ssh_host_rsa_key -C root@#{name} -N ''"
-      @ssh.exec "ssh-keygen -t dsa -f /var/lib/lxc/#{name}/rootfs/etc/ssh/ssh_host_dsa_key -C root@#{name} -N ''"
-      @ssh.exec "ssh-keygen -t ecdsa -f /var/lib/lxc/#{name}/rootfs/etc/ssh/ssh_host_ecdsa_key -C root@#{name} -N ''"
+      @ssh.exec "ssh-keygen -t rsa -f /var/lib/lxc/#{name}/rootfs/etc/ssh/ssh_host_rsa_key -C root@#{name} -N '' -q "
+      @ssh.exec "ssh-keygen -t dsa -f /var/lib/lxc/#{name}/rootfs/etc/ssh/ssh_host_dsa_key -C root@#{name} -N '' -q "
+      @ssh.exec "ssh-keygen -t ecdsa -f /var/lib/lxc/#{name}/rootfs/etc/ssh/ssh_host_ecdsa_key -C root@#{name} -N '' -q"
 
-      @ssh.exec "cp /tmp/iface /var/lib/lxc/#{name}/rootfs/etc/network/interfaces"
+      @ssh.scp "/var/lib/lxc/#{name}/rootfs/etc/network/interfaces", iface
       @ssh.exec "rm /var/lib/lxc/#{name}/rootfs/etc/resolv.conf"
-      @ssh.exec "echo nameserver #{gateway} | sudo tee /var/lib/lxc/#{name}/rootfs/etc/resolv.conf"
+      @ssh.exec "echo nameserver #{network_dns} | sudo tee /var/lib/lxc/#{name}/rootfs/etc/resolv.conf"
 
-      @ssh.exec "chroot /var/lib/lxc/#{name}/rootfs userdel ubuntu"
-      @ssh.exec "chroot /var/lib/lxc/#{name}/rootfs rm -rf /home/ubuntu"
+      # @ssh.exec "chroot /var/lib/lxc/#{name}/rootfs userdel ubuntu"
+      # @ssh.exec "chroot /var/lib/lxc/#{name}/rootfs rm -rf /home/ubuntu"
 
-      @ssh.exec "chroot /var/lib/lxc/#{name}/rootfs useradd #{@cap.fetch(:user)} --shell /bin/bash --create-home --home /home/#{@cap.fetch(:user)}"
-      @ssh.exec "chroot /var/lib/lxc/#{name}/rootfs mkdir /home/#{@cap.fetch(:user)}/.ssh"
-      @ssh.scp "/var/lib/lxc/#{name}/rootfs/home/#{@cap.fetch(:user)}/.ssh/authorized_keys", @params[:ssh_keys].join("\n")
-      @ssh.exec "chroot /var/lib/lxc/#{name}/rootfs chown -R #{@cap.fetch(:user)} /home/#{@cap.fetch(:user)}/.ssh"
+      @ssh.exec "chroot /var/lib/lxc/#{name}/rootfs useradd #{user} --shell /bin/bash --create-home --home /home/#{user}"
+      @ssh.exec "chroot /var/lib/lxc/#{name}/rootfs mkdir /home/#{user}/.ssh"
+      @ssh.scp "/var/lib/lxc/#{name}/rootfs/home/#{user}/.ssh/authorized_keys", ssh_keys.join("\n")
+      @ssh.exec "chroot /var/lib/lxc/#{name}/rootfs chown -R #{user} /home/#{user}/.ssh"
       @ssh.exec "echo 'chef   ALL=(ALL) NOPASSWD:ALL' | sudo tee -a /var/lib/lxc/#{name}/rootfs/etc/sudoers"
 
-      @ssh.exec "chroot /var/lib/lxc/#{name}/rootfs apt-get install curl -y"
-
-      @ssh.exec "umount /dev/#{@params[:vg_name]}/#{name}"
-      @ssh.exec "rm /tmp/config"
-      @ssh.exec "rm /tmp/iface"
+      @ssh.exec "umount /dev/#{lvm_vg}/#{name}" if lvm_mode
+      @ssh.exec "rm /tmp/lxc_config"
       @ssh.exec "lxc-start -d -n #{name}"
       @ssh.exec "ln -s /var/lib/lxc/#{name}/config /etc/lxc/auto/#{name}"
-      wait_ssh ip_config[:ip], @cap.fetch(:user)
+      wait_ssh ip_config[:ip], user
     end
   end
 
