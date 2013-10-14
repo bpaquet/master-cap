4a28cba6a648e9ef8cea56dcc5d7175b01415ac4
Bertrand Paquet <bertrand.paquet@gmail.com>
2013-10-14 22:11:18 +0200

    Use last version for bootstraping lxc vm

--- cookbooks/master_cap_lxc/attributes/default.rb
+++ cookbooks/master_cap_lxc/attributes/default.rb
@@ -1,3 +1,3 @@
 
 default[:master_cap_lxc][:chef_url] = "http://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/11.04/x86_64/chef_11.6.0-1.ubuntu.11.04_amd64.deb"
-default[:master_cap_lxc][:master_chef_url] = "https://launchpad.net/~bpaquet/+archive/master-chef/+files/master-chef_1.0-4_all.deb"
+default[:master_cap_lxc][:master_chef_url] = "https://launchpad.net/~bpaquet/+archive/master-chef/+files/master-chef_1.0-5_all.deb"
